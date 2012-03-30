7926d46a8514de5e36a5ddc5e27b27ca7eeba794
Tyraen <mordimmer@gmail.com>
2012-03-30 17:00:55 +0200

    little update

--- Mainframe.java
+++ Mainframe.java
@@ -1,5 +1,35 @@
 package hwc;
 
-public class Mainframe {
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
+public class Mainframe extends JFrame 
+{
+	JFrame gui;
+	
+	public static void main(String[]args)
+	{
+		new Mainframe();
+	}
+	public Mainframe()
+	{
+		try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+        } catch (InstantiationException ex) {
+        } catch (IllegalAccessException ex) {
+        } catch (UnsupportedLookAndFeelException ex){
+        }
+		init();
+	}
+	public void init()
+	{
+		gui = new JFrame();
+		gui.setSize(800,600);
+		gui.setTitle("Hand Writing Recognition");
+		gui.setVisible(true);
+		gui.setLocationRelativeTo(null);
+		gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
